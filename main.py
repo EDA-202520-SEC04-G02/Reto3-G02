4f8fb3c5eaf3ab9d0d33d24dbb97e54f8d2c9a76
LuigiToby <la.rodrigueza12@uniandes.edu.co>
2025-11-09 14:34:10 -0500

    sol problemas csv y recursion

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import App.view as view
 
+# Solucionar problemas de buffer
+import csv
+csv.field_size_limit(2147483647)
+# Limite que la guía recomienda cambiar
+import sys
+default_limit = 1000
+sys.setrecursionlimit(default_limit*10)
 
 # Main function
 def main():
